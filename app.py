bbd09d278a6b601e998e560e5b168cbc395e3666
brunoHenrique2410 <93950098+brunoHenrique2410@users.noreply.github.com>
2025-08-12 16:36:51 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 from datetime import date, time
 from PIL import Image
 import io
-import fitz  # PyMuPDF
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import A4
 from reportlab.lib.utils import ImageReader
@@ -118,3 +117,4 @@ if st.button("Gerar PDF Preenchido"):
     )
 
     st.success("✅ PDF gerado com sucesso!")
+
