ddc9e72ee548564e765998a2071fb94f93301202
brunoHenrique2410 <93950098+brunoHenrique2410@users.noreply.github.com>
2026-04-28 11:25:23 -0300

    Update state.py

--- common/state.py
+++ common/state.py
@@ -1,27 +1,13 @@
 import streamlit as st
-from urllib.parse import urlencode
 
-def init_defaults(defaults: dict):
-    ss = st.session_state
-    for k, v in defaults.items():
-        if k not in ss:
-            ss[k] = v
+def init_defaults():
+    defaults = {
+        "equipamentos": [],
+        "fotos": [],
+        "assinatura_tecnico": None,
+        "assinatura_cliente": None,
+    }
 
-BASIC_KEYS = ["cliente", "numero_chamado", "hora_inicio", "hora_termino"]
-
-def get_initial_payload_url(base_url: str) -> str:
-    params = {}
-    ss = st.session_state
-    for k in BASIC_KEYS:
-        val = ss.get(k)
-        if val is None:
-            continue
-        params[k] = str(val)
-    return f"{base_url}?{urlencode(params)}"
-
-def load_from_query_params():
-    qs = st.query_params
-    ss = st.session_state
-    for k in BASIC_KEYS:
-        if k in qs and k not in ss:
-            ss[k] = qs.get(k)
+    for key, value in defaults.items():
+        if key not in st.session_state:
+            st.session_state[key] = value
