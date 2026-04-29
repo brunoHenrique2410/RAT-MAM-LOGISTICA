0d03f0d7ed65f2b7783f6b177a39fa030b23a0a6
brunoHenrique2410 <93950098+brunoHenrique2410@users.noreply.github.com>
2026-04-29 07:29:21 -0300

    Update app.py

--- repo/app.py
+++ repo/app.py
@@ -1,5 +1,20 @@
-# repo/app.py
+import os
+import sys
 import streamlit as st
+
+st.write("CWD:", os.getcwd())
+st.write("__file__:", __file__)
+st.write("sys.path:", sys.path[:5])
+st.write("Arquivos na pasta atual:", os.listdir("."))
+st.write("Arquivos em /repo:", os.listdir(os.path.dirname(__file__)))
+
+repo_dir = os.path.dirname(__file__)
+common_dir = os.path.join(repo_dir, "common")
+
+st.write("Existe common?", os.path.exists(common_dir))
+if os.path.exists(common_dir):
+    st.write("Arquivos em common:", os.listdir(common_dir))
+
 import rat_unificado
 def main():
     st.set_page_config(
